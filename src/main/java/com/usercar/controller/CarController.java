14eb2659b627f8eaa2b6baacfdb4c68de5a2ac77
RochitJain <47205340+RochitJain@users.noreply.github.com>
2020-12-10 19:33:40 +0530

    Update CarController.java

--- src/main/java/com/usercar/controller/CarController.java
+++ src/main/java/com/usercar/controller/CarController.java
@@ -18,6 +18,7 @@ import com.usercar.model.Trip;
 import com.usercar.service.CarService;
 
 @RestController
+@CrossOrigin(origins ="http://localhost:19006")
 public class CarController {
 
 	@Autowired
@@ -43,7 +44,6 @@ public class CarController {
 	{
 		return new ResponseEntity<>(carservice.addCarTrip(token, carno, carTrip),HttpStatus.OK);
 	}
-	@CrossOrigin(origins ="*")
 	@RequestMapping(method=RequestMethod.GET,path="/viewcar/{carno}")
 	public ResponseEntity<?> viewCar(@PathVariable("carno")String carno,@RequestHeader(name="Authorization")String token)
 	{
