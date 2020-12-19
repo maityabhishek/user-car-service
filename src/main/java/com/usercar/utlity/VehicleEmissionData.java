dffca97d9417c5d2d6dd3c1c6c762ca8870f6119
amolmane123 <51505215+amolmane123@users.noreply.github.com>
2020-12-19 11:36:40 +0530

    Update VehicleEmissionData.java

--- src/main/java/com/usercar/utlity/VehicleEmissionData.java
+++ src/main/java/com/usercar/utlity/VehicleEmissionData.java
@@ -7,7 +7,17 @@ public class VehicleEmissionData implements CarDataGenerator {
 
 	@Override
 	public Message generateData(String simulationId) {
-		String msgStr = "{\"co2level\":"+ ((int)(1500*Math.random())) +",\"usercase\":\"VehicleEmission\"" +",\"nolevel\":"+ ((int)(80*Math.random()))+ ",\"vin\":"+ ((int)(10000*Math.random()))+"}";
+		Random r = new Random();
+		int low = 800;
+		int high = 1200;
+		int co2level = r.nextInt(high-low) + low;
+		
+		Random r1 = new Random();
+		int low1 = 45;
+		int high1 = 70;
+		int nolevel = r1.nextInt(high1-low1) + low1;
+				
+		String msgStr = "{\"co2level\":"+ co2level +",\"usercase\":\"VehicleEmission\"" +",\"nolevel\":"+ nolevel + ",\"vin\":"+ ((int)(10000*Math.random()))+"}";
 		Message msg = new Message(msgStr);
 		System.out.println(msgStr);
 		return msg;
