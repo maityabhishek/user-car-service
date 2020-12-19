a792d8dbd9124ce5f03e1875cf055a1192396d60
amolmane123 <51505215+amolmane123@users.noreply.github.com>
2020-12-19 11:33:42 +0530

    Update VehicleConditionData.java

--- src/main/java/com/usercar/utlity/VehicleConditionData.java
+++ src/main/java/com/usercar/utlity/VehicleConditionData.java
@@ -8,7 +8,11 @@ public class VehicleConditionData implements CarDataGenerator {
 	@Override
 	public Message generateData(String simulationId)
 	{
-		String msgStr = "{\"coolanttemp\":" + ((int)(200*Math.random()%150)) +",\"usercase\":\"VehicleCondition\"" +",\"vin\":"+ ((int)(10000*Math.random()))+",\"engineload\":" + ((int)(100*Math.random()%100)) + "}";
+		Random r = new Random();
+		int low = 80;
+		int high = 120;
+		int coolanttemp = r.nextInt(high-low) + low;		
+		String msgStr = "{\"coolanttemp\":" + coolanttemp +",\"usercase\":\"VehicleCondition\"" +",\"vin\":"+ ((int)(10000*Math.random()))+",\"engineload\":" + ((int)(100*Math.random()%100)) + "}";
 		Message msg = new Message(msgStr);
 		System.out.println(msgStr);
 		return msg;
