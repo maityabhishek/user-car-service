66d6bab0627f1eca9aaa073282f76ff5532ac39a
amolmane123 <51505215+amolmane123@users.noreply.github.com>
2020-12-19 11:38:28 +0530

    Update VehicleTripData.java

--- src/main/java/com/usercar/utlity/VehicleTripData.java
+++ src/main/java/com/usercar/utlity/VehicleTripData.java
@@ -13,7 +13,12 @@ public class VehicleTripData implements CarDataGenerator {
 	}
 	@Override
 	public Message generateData(String simulationId) {
-		String msgStr = "{\"speed\":"+ ((int)(1000*Math.random()%180))+",\"simulationid\":\""+simulationId+"\",\"usercase\":\"VehicleTrip\""+",\"tripid\":"+ (1) +",\"longitude\":"+ ((int)(100*Math.random()))+",\"latitude\":"+ ((int)(100*Math.random()))+",\"vin\":"+ (100)+",\"fuel\":"+ (fuel)+",\"odometer\":"+ (kms++)+",\"timestamp\":"+ (new Date().getTime())+",\"rpm\":"+ ((int)(10000*Math.random()%2500))+"}";
+		Random r = new Random();
+		int low = 60;
+		int high = 100;
+		int speed = r.nextInt(high-low) + low;
+				
+		String msgStr = "{\"speed\":"+ speed +",\"simulationid\":\""+simulationId+"\",\"usercase\":\"VehicleTrip\""+",\"tripid\":"+ (1) +",\"longitude\":"+ ((int)(100*Math.random()))+",\"latitude\":"+ ((int)(100*Math.random()))+",\"vin\":"+ (100)+",\"fuel\":"+ (fuel)+",\"odometer\":"+ (kms++)+",\"timestamp\":"+ (new Date().getTime())+",\"rpm\":"+ ((int)(10000*Math.random()%2500))+"}";
 		fuel=fuel-0.06;
 		Message msg = new Message(msgStr);
 		System.out.println(msgStr);
