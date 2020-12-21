091890f826077a7de5a62950058c528f80bd614f
Abhishek Maity <a.maity@outlook.com>
2020-12-21 15:33:46 +0530

    Update UserCarServiceApplication.java

--- src/main/java/com/usercar/UserCarServiceApplication.java
+++ src/main/java/com/usercar/UserCarServiceApplication.java
@@ -27,7 +27,7 @@ public class UserCarServiceApplication {
 	}
 
 }
-/*
+
 @Component
 class cosmosdb
 {
@@ -36,18 +36,17 @@ class cosmosdb
 	{
 		
 		cardao.save(new Car("OD02F7497","100","OD001","A","M","Black",new Date(),3000,56.9,0,0));
-		//carservdao.save(new CarServicing("OD02F7497", "PLG H", new Date(), "All Ok "));
-		//carpucdao.save(new CarPuc("OD02F7497", new Date(), new Date(), "Bhubaneswar", "Active"));
-		//List<TripSplits> ts= new ArrayList<TripSplits>();
-		//ts.add(new TripSplits(1,10,10.2,15,1));
-		//ts.add(new TripSplits(11,20,10.2,15,1));
-		//cartrip.save(new Trip(1,"OD02F7497", 658.3, 30.2, 45.3, 3.1,"Mumbai","Pune",new Date(),10,ts));
-		 //ts= new ArrayList<TripSplits>();
-		//ts.add(new TripSplits(1,10,10.2,15,1));
-		//ts.add(new TripSplits(11,20,10.2,15,1));
-		//ts.add(new TripSplits(21,31,10.2,15,1));
-		//cartrip.save(new Trip(2,"OD02F7497", 1658.3, 60.2, 93.3, 7.1,"Delhi","Pune",new Date(),10,ts));
+		carservdao.save(new CarServicing("OD02F7497", "PLG H", new Date(), "All Ok "));
+		carpucdao.save(new CarPuc("OD02F7497", new Date(), new Date(), "Bhubaneswar", "Active"));
+		List<TripSplits> ts= new ArrayList<TripSplits>();
+		ts.add(new TripSplits(1,10,10.2,15,1));
+		ts.add(new TripSplits(11,20,10.2,15,1));
+		cartrip.save(new Trip(1,"OD02F7497", 658.3, 30.2, 45.3, 3.1,"Mumbai","Pune",new Date(),10,ts));
+		ts= new ArrayList<TripSplits>();
+		ts.add(new TripSplits(1,10,10.2,15,1));
+		ts.add(new TripSplits(11,20,10.2,15,1));
+		ts.add(new TripSplits(21,31,10.2,15,1));
+		cartrip.save(new Trip(2,"OD02F7497", 1658.3, 60.2, 93.3, 7.1,"Delhi","Pune",new Date(),10,ts));
 	}
 }
 
-*/
