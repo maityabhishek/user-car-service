dc47bfe7b4f88ea854c20f75111ee490a54842d4
Abhishek Maity <a.maity@outlook.com>
2020-12-22 11:05:50 +0530

    Update VehicleConditionData.java

--- src/main/java/com/usercar/utlity/VehicleConditionData.java
+++ src/main/java/com/usercar/utlity/VehicleConditionData.java
@@ -13,7 +13,7 @@ public class VehicleConditionData implements CarDataGenerator {
 		int low = 80;
 		int high = 120;
 		int coolanttemp = r.nextInt(high-low) + low;		
-		String msgStr = "{\"coolanttemp\":" + coolanttemp +",\"usercase\":\"VehicleCondition\"" +",\"vin\":"+ ((int)(10000*Math.random()))+",\"engineload\":" + ((int)(100*Math.random()%100)) + "}";
+		String msgStr = "{\"coolanttemp\":" + coolanttemp +",\"usercase\":\"VehicleCondition\"" +",\"vin\":"+ (100)+",\"engineload\":" + ((int)(100*Math.random()%100)) + "}";
 		Message msg = new Message(msgStr);
 		System.out.println(msgStr);
 		return msg;
